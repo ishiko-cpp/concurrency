e0f61007ea1a0a1ca3e8a8c36cd86679fb9a22a8
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-05-26 13:20:13 +0100

    Add files

---
+++ include/Ishiko/Concurrency.hpp
@@ -0,0 +1,12 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/concurrency/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_CONCURRENCY_HPP_
+#define _ISHIKO_CPP_CONCURRENCY_HPP_
+
+#include "Concurrency/linkoptions.hpp"
+
+#endif
